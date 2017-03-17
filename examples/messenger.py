05228deff7e2a453b133470a326e98152fc803a0
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-17 11:22:24 +0200

    Update messenger.py

--- examples/messenger.py
+++ examples/messenger.py
@@ -24,11 +24,11 @@ from wit import Wit
 from bottle import Bottle, request, debug
 
 # Wit.ai parameters
-WIT_TOKEN = os.environ.get('WIT_TOKEN')
+WIT_TOKEN = os.environ.get('2K7GXJSRTF7B5ZB74PNZBZQPGFYIIRKC')
 # Messenger API parameters
-FB_PAGE_TOKEN = os.environ.get('FB_PAGE_TOKEN')
+FB_PAGE_TOKEN = os.environ.get('EAAD0FIbuQqcBAGGBjgkx8bqZAHqD0Upa37c1yzxHW4r3j9b3uR3ExS0PI8ZB8mBVShJBm369cZA9Cmc3iJB6LchZAH4eLLuypj6Ge8Se9dQZBmke7UiAGPZAdZBT4eXJTctsVqSLiYwtdFavvS3GCu2mkbSJ6PvnDnrZANboZClKUhwZDZD')
 # A user secret to verify webhook get request.
-FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
+FB_VERIFY_TOKEN = os.environ.get('banana')
 
 # Setup Bottle Server
 debug(True)
